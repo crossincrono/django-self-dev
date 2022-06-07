f7b5db75d4c7b699b201ff6ab8861ec465721e68
Bounthavy Jay Thanpirak <59497672+crossincrono@users.noreply.github.com>
2022-06-07 01:23:34 -0700

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://jthanpirak-frontend.herokuapp.com/']
+#CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://jthanpirak-frontend.herokuapp.com/']
 
 # Application definition
 
